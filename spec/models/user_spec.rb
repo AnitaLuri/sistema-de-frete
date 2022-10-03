cf6fcf9c01a1a893ebc4a78449f3be49f087b92f
Anita Luri <anitaluri.h@gmail.com>
2022-10-03 16:17:48 -0300

    Validacao cadastro de usuario

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- 
+  describe '#valid?' do
+    it 'falso quando o nome fica em branco' do
+      #Arrange
+      user = User.new(name: '', email: 'teste@sistemadefrete.com.br', password: 'password')
+      #Act
+      #Assert
+      expect(user.valid?).to eq false
+    end
+    it 'falso quando o email fica em branco' do
+      #Arrange
+      user = User.new(name: 'Maria', email: '', password: 'password')
+      #Act
+      #Assert
+      expect(user.valid?).to eq false
+    end
+    it 'falso quando o senha fica em branco' do
+      #Arrange
+      user = User.new(name: 'Maria', email: 'teste@sistemadefrete.com.br', password: '')
+      #Act
+      #Assert
+      expect(user.valid?).to eq false
+    end
+    it 'falso quando o email é de outro domínio' do
+      #Arrange
+      user = User.new(name: 'Maria', email: 'teste@qualquerdominio.com.br', password: 'password')
+      other_user = User.new(name: 'Maria', email: 'teste@sistemadefrete.com.br', password: 'password')
+      #Act
+      #Assert
+      expect(user.valid?).to eq false
+      expect(other_user.valid?).to eq true
+    end
+  end
 end
--- spec/system/user/user_sign_up_spec.rb
+++ spec/system/user/user_sign_up_spec.rb
@@ -27,11 +27,42 @@ describe 'Usuário se cadastra' do
     click_on 'Entrar'
     click_on 'Criar conta'
     fill_in 'Nome', with: ''
+    fill_in 'E-mail', with: ''
+    fill_in 'Senha', with: ''
+    fill_in 'Confirme sua senha', with: ''
+    click_on 'Criar Usuário'
+    #Assert
+    expect(page).to have_content 'Nome não pode ficar em branco'
+    expect(page).to have_content 'E-mail não pode ficar em branco'
+    expect(page).to have_content 'Senha não pode ficar em branco'
+  end
+  it 'com email invalido' do
+    #Arrange
+    #Act
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar conta'
+    fill_in 'Nome', with: 'Maria'
+    fill_in 'E-mail', with: 'teste@frete.com.br'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    click_on 'Criar Usuário'
+    #Assert
+    expect(page).to have_content 'E-mail não é válido'
+  end
+  it 'com email único' do
+    #Arrange
+    User.create!(name: 'Maria', email: 'teste@sistemadefrete.com.br', password: 'password')
+    #Act
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar conta'
+    fill_in 'Nome', with: 'João'
     fill_in 'E-mail', with: 'teste@sistemadefrete.com.br'
     fill_in 'Senha', with: 'password'
     fill_in 'Confirme sua senha', with: 'password'
     click_on 'Criar Usuário'
     #Assert
-    expect(page).to have_content 'Nome não pode ficar em branco'
+    expect(page).to have_content 'E-mail já está em uso'
   end
 end
