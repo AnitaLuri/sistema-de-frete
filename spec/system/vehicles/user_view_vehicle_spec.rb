a6ad90ca03d1a31491f1f278662336423f046619
Anita Luri <anitaluri.h@gmail.com>
2022-10-15 15:34:46 -0300

    Adicao do status no index de veiculos

--- spec/system/vehicles/user_view_vehicle_spec.rb
+++ spec/system/vehicles/user_view_vehicle_spec.rb
@@ -17,9 +17,9 @@ describe 'Usuário vê frota de veículos' do
     user = User.create!(name: 'Maria', email: 'teste@sistemadefrete.com.br', password: 'password', profile: 5)
     transport_mode = TransportMode.create!(name: 'Caminhão', minimum_distance: 100, maximum_distance: 5000, minimum_weight: 50000, maximum_weight: 5000000, fixed_value: '500,00') 
     Vehicle.create!(plate: 'ABC4252', brand: 'Ford', model: 'F350', year: 2014, 
-                    weight_limit: 50000, transport_mode: transport_mode)
+                    weight_limit: 50000, transport_mode: transport_mode, status: 0)
     Vehicle.create!(plate: 'FGH5968', brand: 'Scania', model: 'P310', year: 2015, 
-                    weight_limit: 40000, transport_mode: transport_mode)
+                    weight_limit: 40000, transport_mode: transport_mode, status: 3)
   
     login_as(user)
     visit root_path
@@ -27,8 +27,9 @@ describe 'Usuário vê frota de veículos' do
 
     expect(page).to have_content 'Frota de Veículos'
     expect(page).to have_content 'Placa: ABC4252'
+    expect(page).to have_content 'Situação: Ativo'
     expect(page).to have_content 'Modalidade de Transporte: Caminhão'
     expect(page).to have_content 'Placa: FGH5968'
-    expect(page).to have_content 'Modalidade de Transporte: Caminhão'
+    expect(page).to have_content 'Situação: Em operação'
   end
 end
